eb4ec3b35fbded07139ae00a63a4310293c1f39a
Fasel Godbcho <faselergando2000@gmail.com>
2024-03-23 21:24:00 +0300

    Fixed some UI glitches and bugs

--- JS/main.js
+++ JS/main.js
@@ -5,11 +5,12 @@ window.addEventListener('DOMContentLoaded', e => {
 });
 
 function initApp() {
-  //Dom Elements
+  //DOM Elements
   const searchSiteInput = document.querySelector('.search-input-toggle');
   const closeSearchModal = document.querySelector('.close-search-modal');
   const searchSiteBtn = document.querySelector('.search-btn');
-  const headerLinkNav = document.querySelectorAll('.header-nav-links li');
+  const headerLinkNav = document.querySelectorAll('.header-nav-links:not(.header-nav-links-sm) li');
+  const headerLinkNav_Sm = document.querySelectorAll('.header-nav-links-sm li');
 
   searchSiteInput.addEventListener('click', e => {
     searchSiteInput.classList.add('scale-0');
@@ -23,8 +24,14 @@ function initApp() {
   headerLinkNav.forEach(link => {
     link.addEventListener('click', e => {
       headerLinkNav.forEach(ln => ln.classList.remove('header-link-active'));
-
       link.classList.add('header-link-active');
     });
-  })
+  });
+
+  headerLinkNav_Sm.forEach(link => {
+    link.addEventListener('click', e => {
+      headerLinkNav_Sm.forEach(ln => ln.classList.remove('header-link-active'));
+      link.classList.add('header-link-active');
+    });
+  });
 }
